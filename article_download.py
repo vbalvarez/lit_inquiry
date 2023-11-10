6b5959cf2fd6d2dce805f335ea841f7c295386dc
Vini Alvarez <114950091+vbalvarez@users.noreply.github.com>
2023-11-10 11:05:13 -0600

    Update article_download.py
    - renames articles

--- article_download.py
+++ article_download.py
@@ -12,6 +12,7 @@ from time import sleep
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 import argparse
+import time
 
 # Command call for directory and files
 def parse_args():
@@ -22,36 +23,46 @@ def parse_args():
                         help='Directory where PDFs will be downloaded')
     return parser.parse_args()
 
-# Given DOIs list, downloads PDFs to folder_path
+# Download file give df
 def download_pdfs(df, folder_path):
     os.makedirs(folder_path, exist_ok=True)
-    os.chmod(folder_path, mode = 0o777)
-    
+    os.chmod(folder_path, mode=0o777)
+
     # Browser options
     browser_options = ChromeOptions()
     browser_options.add_experimental_option('prefs', {
-        "download.default_directory": folder_path, # Change default directory for downloads
-        "download.prompt_for_download": False, # To auto download the file
+        "download.default_directory": folder_path,
+        "download.prompt_for_download": False,
         "download.directory_upgrade": True,
-        "plugins.always_open_pdf_externally": True # It will not show PDF directly in chrome
+        "plugins.always_open_pdf_externally": True
     })
     service = ChromeService(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=browser_options)
-
-    aea_df = df[df['Publisher'] == 'American Economic *']
     
-    # Loop over DOIs and get files
+    df['DOI_link'].fillna('NA')
+
+    aea_df = df[(df['Publisher'] == 'American Economic *') & (pd.notna(df['DOI_link']))]
+
     for doi in aea_df['DOI_link']:
-        if doi != "NA":
-            if 'https://' not in doi:
-                print("Downloading file from link: {}".format("https://pubs.aeaweb.org/doi/pdfplus/" + doi))
-                driver.get("https://pubs.aeaweb.org/doi/pdfplus/" + doi)
-                sleep(2)
-            else:
-                print("Downloading file from link: {}".format(doi))
-                driver.get(doi)
-                sleep(2)
-        
+        print(doi)
+        download_url = "https://pubs.aeaweb.org/doi/pdfplus/" + doi if 'https://' not in doi else doi
+        print(f"Downloading file from link: {download_url}")
+        driver.get(download_url)
+        sleep(2)  # Wait for the download to start
+
+        # Wait for the download to finish
+        while any([filename.endswith(".crdownload") for filename in os.listdir(folder_path)]):
+            time.sleep(1)  # Check every second
+
+        # Rename the file
+        downloaded_files = os.listdir(folder_path)
+        if downloaded_files:
+            # Assuming the latest file in the directory is the one we just downloaded
+            latest_file = max([os.path.join(folder_path, f) for f in downloaded_files], key=os.path.getctime)
+            new_file_name = os.path.join(folder_path, doi.replace('/', '_') + '.pdf')
+            os.rename(latest_file, new_file_name)
+            print(f"Renamed downloaded file to {new_file_name}")
+
     driver.quit()
 
 # Usage
